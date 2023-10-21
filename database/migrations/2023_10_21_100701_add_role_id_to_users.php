658f00cf73f464dd658c2ce3cd98c080e70d39c6
Andi Surya <andi502.as@gmail.com>
2023-10-21 17:12:23 +0700

    add roles table and relationship

---
+++ database/migrations/2023_10_21_100701_add_role_id_to_users.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('role_id')->nullable();
+
+            // Add a foreign key constraint to reference the roles table
+            $table->foreign('role_id')->references('id')->on('user_roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['role_id']); // Remove the foreign key constraint
+            $table->dropColumn('role_id'); // Remove the column
+        });
+    }
+};
